c203c5436f79450f30811a45ac78d28755d45f15
Michael <michaelb199999@gmail.com>
2024-06-10 02:34:24 +0100

    start on ast

---
+++ src/ast/ast.ts
@@ -0,0 +1,39 @@
+import type { Token } from "../token/token";
+
+export interface Node {
+	tokenLiteral(): string;
+}
+export interface Statement extends Node {
+	statementNode(): void;
+}
+export interface Expression extends Node {
+	expressionNode(): void;
+}
+
+export class Program implements Node {
+	constructor(public statements: Statement[]) {}
+	tokenLiteral(): string {
+		if (this.statements.length > 0) {
+			return this.statements[0].tokenLiteral();
+		}
+		return "";
+	}
+}
+
+export class LetStatement implements Statement {
+	public name: Identifier | null = null;
+	public value: string | null = null;
+	constructor(public token: Token) {}
+	statementNode() {}
+	tokenLiteral(): string {
+		return this.token.literal;
+	}
+}
+
+export class Identifier implements Expression {
+	constructor(public token: Token) {}
+	expressionNode() {}
+	tokenLiteral(): string {
+		return this.token.literal;
+	}
+}
